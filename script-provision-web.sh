0a5ee46171e808ae87ec7c11d3dd96c61b8e64b7
Júnior César Abreu <juniorcesarabreu@live.com>
2022-08-25 19:46:56 +0000

    Alteração de apt para apt-get e pequenos ajustes

--- script-provision-web.sh
+++ script-provision-web.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
 echo "Atualizando o sistema..."
-apt update -y
-apt upgrade -y
+apt-get update -y
+apt-get upgrade -y
 echo "Sistema atualizado!"
 
 echo "Instalando o Apache..."
-apt install apache2 -y
+apt-get install apache2 -y
 systemctl start apache2
 echo "Apache instalado!"
 
-echo "Instalando o UnZip..."
-apt install unzip -y
-echo "UnZip instalado!"
+echo "Instalando o UnZip e Wget..."
+apt-get install unzip wget -y
+echo "UnZip e Wget instalado!"
 
 echo "Baixando a aplicação..."
 wget -P /tmp/ https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 echo "Descompactando..."
-unzip /tmp/main.zip -d /tmp/
+unzip -q /tmp/main.zip -d /tmp/
 rm -r -f /tmp/main.zip
 echo "Movendo para o diretório do servidor web..."
+rm -r -f /var/www/html/*
 cp -r -f /tmp/linux-site-dio-main/* /var/www/html/
 rm -r -f /tmp/linux-site-dio-main/
 echo "Aplicação instalada!"
